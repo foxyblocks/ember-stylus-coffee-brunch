f518637486e6f46013ab19026cd6c0d34458463d
Christian <christiantx@gmail.com>
2012-11-17 08:33:55 -0600

    update ember-data

--- vendor/scripts/ember-data.js
+++ vendor/scripts/ember-data.js
@@ -4503,27 +4503,112 @@ Ember.onLoad('Ember.Application', function(Application) {
 
 
 (function() {
-var get = Ember.get, set = Ember.set;
+DS.Transforms = Ember.Object.extend({
+  string: {
+    fromJSON: function(serialized) {
+      return Ember.none(serialized) ? null : String(serialized);
+    },
 
-var passthrough = {
-  fromJSON: function(value) {
-    return value;
+    toJSON: function(deserialized) {
+      return Ember.none(deserialized) ? null : String(deserialized);
+    }
   },
 
-  toJSON: function(value) {
-    return value;
+  number: {
+    fromJSON: function(serialized) {
+      return Ember.none(serialized) ? null : Number(serialized);
+    },
+
+    toJSON: function(deserialized) {
+      return Ember.none(deserialized) ? null : Number(deserialized);
+    }
+  },
+
+  // Handles the following boolean inputs:
+  // "TrUe", "t", "f", "FALSE", 0, (non-zero), or boolean true/false
+  'boolean': {
+    fromJSON: function(serialized) {
+      var type = typeof serialized;
+
+      if (type === "boolean") {
+        return serialized;
+      } else if (type === "string") {
+        return serialized.match(/^true$|^t$|^1$/i) !== null;
+      } else if (type === "number") {
+        return serialized === 1;
+      } else {
+        return false;
+      }
+    },
+
+    toJSON: function(deserialized) {
+      return Boolean(deserialized);
+    }
+  },
+
+  date: {
+    fromJSON: function(serialized) {
+      var type = typeof serialized;
+
+      if (type === "string" || type === "number") {
+        // this is a fix for Safari 5.1.5 on Mac which does not accept timestamps as yyyy-mm-dd
+        if (serialized.search(/^\d{4}-\d{2}-\d{2}$/) !== -1){
+          serialized += "T00:00:00Z";
+        }
+
+        return new Date(serialized);
+      } else if (serialized === null || serialized === undefined) {
+        // if the value is not present in the data,
+        // return undefined, not null.
+        return serialized;
+      } else {
+        return null;
+      }
+    },
+
+    toJSON: function(date) {
+      if (date instanceof Date) {
+        var days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+        var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+        var pad = function(num) {
+          return num < 10 ? "0"+num : ""+num;
+        };
+
+        var utcYear = date.getUTCFullYear(),
+            utcMonth = date.getUTCMonth(),
+            utcDayOfMonth = date.getUTCDate(),
+            utcDay = date.getUTCDay(),
+            utcHours = date.getUTCHours(),
+            utcMinutes = date.getUTCMinutes(),
+            utcSeconds = date.getUTCSeconds();
+
+
+        var dayOfWeek = days[utcDay];
+        var dayOfMonth = pad(utcDayOfMonth);
+        var month = months[utcMonth];
+
+        return dayOfWeek + ", " + dayOfMonth + " " + month + " " + utcYear + " " +
+               pad(utcHours) + ":" + pad(utcMinutes) + ":" + pad(utcSeconds) + " GMT";
+      } else if (date === undefined) {
+        return undefined;
+      } else {
+        return null;
+      }
+    }
   }
-};
+});
+})();
+
+
+
+(function() {
+var get = Ember.get, set = Ember.set;
 
 DS.Serializer = Ember.Object.extend({
   init: function() {
-    // By default, the JSON types are passthrough transforms
-    this.transforms = {
-      'string': passthrough,
-      'number': passthrough,
-      'boolean': passthrough
-    };
-
+    // By default, the JSON types are the transforms defined in DS.Transforms
+    this.transforms = DS.Transforms.create();
     this.mappings = Ember.Map.create();
   },
 
